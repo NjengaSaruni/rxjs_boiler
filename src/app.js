f6ca869b8b6204596ee3412128491f824461919e
Peter Njenga <njengasaruni@gmail.com>
2018-12-28 12:56:45 +0300

    Observables from events

--- src/app.js
+++ src/app.js
@@ -1,4 +1,49 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+const btn = $('#button');
+const input = $('#input');
+const output = $('#output');
+const btnStream$ = Rx.Observable.fromEvent(btn, 'click');
+
+btnStream$.subscribe(
+    function (event) {
+        console.log(event.target.innerHTML);
+    },
+    function (err) {
+        console.log(err);
+    },
+    function() {
+        console.log('Completed');
+    }
+);
+
+const inputStream$ = Rx.Observable.fromEvent(input, 'keyup');
+
+inputStream$.subscribe(
+    function (event) {
+        console.log(event.target.value);
+        output.append(event.target.value);
+    },
+    function (err) {
+        console.log(err);
+    },
+    function() {
+        console.log('Completed');
+    }
+);
+
+const mouseMoveStream$ = Rx.Observable.fromEvent(output, 'mousemove');
+
+mouseMoveStream$.subscribe(
+    function (event) {
+        output.html(`<h1>X :${event.clientX} Y: ${event.clientY}</h1>`);
+        console.log(event.clientX);
+    },
+    function (err) {
+        console.log(err);
+    },
+    function() {
+        console.log('Completed');
+    }
+);
